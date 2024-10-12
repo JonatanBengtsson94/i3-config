c4d5d201db981bf92852025c345f70b16a04e7c7
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-10-12 19:50:37 +0200

    Started work on install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Update package list
+echo "Updating package list..."
+sudo apt update
+
+# Install Xorg
+echo "Installing Xorg..."
+sudo apt install -y xorg
+
+# Install i3
+sudo apt install -y i3
