50400c3ee62660f420c89b00d27433c45ab53714
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-10-13 13:22:54 +0200

    added customization

--- install.sh
+++ install.sh
@@ -2,11 +2,34 @@
 echo "Updating package list..."
 sudo apt update
 
-# Install Xorg
-echo "Installing Xorg..."
+## Installation
+
+# Display server
 sudo apt install -y xorg
 
-# Install i3
-echo "Installing i3"
+# Window manager
 sudo apt install -y i3 i3blocks
+
+# Terminal
+sudo apt install -y kitty
+
+# Compositor
+sudo apt install -y picom
+
+# Background setter
+sudo apt install -y nitrogen
+
+## Customization
+
+# Create config dirs
+mkdir -p ~/.config/i3/ ~/.config/i3blocks/ ~/.config/kitty/ ~/.config/picom/
+
+# Copy config files
+cp -r ./i3 ~/.config/i3/
+cp -r ./i3blocks ~/.config/i3blocks/
+cp -r ./kitty ~/.config/kitty/
+cp -r ./picom ~/.config/picom/
+
+echo "Installation complete, rebooting now"
+sudo shutdown -r +0.05
 
