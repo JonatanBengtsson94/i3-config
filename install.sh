2901b8b187e643224e3539ad0f49e8f4e004c873
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-10-13 12:10:16 +0200

    added i3 utils

--- install.sh
+++ install.sh
@@ -7,4 +7,9 @@ echo "Installing Xorg..."
 sudo apt install -y xorg
 
 # Install i3
+echo "Installing i3"
 sudo apt install -y i3
+
+# Install i3 utils
+echo "Installing i3bar, i3status, i3blocks"
+sudo apt install -y i3bar i3status i3blocks
