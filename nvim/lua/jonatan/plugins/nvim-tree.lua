5083b769b8e56194f1c4639b798e6eeea4074f47
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-10-16 16:10:36 +0200

    added dev-icons

--- nvim/lua/jonatan/plugins/nvim-tree.lua
+++ nvim/lua/jonatan/plugins/nvim-tree.lua
@@ -1,24 +1,25 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  config = function()
-    local nvimtree = require("nvim-tree")
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local nvimtree = require("nvim-tree")
 
-    -- recommended settings
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+		-- recommended settings
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    nvimtree.setup({
-      view = {
-        width = 35,
-        relativenumber = true,
-      },
-      git = {
-        ignore = false,
-      },
-    })
+		nvimtree.setup({
+			view = {
+				width = 35,
+				relativenumber = true,
+			},
+			git = {
+				ignore = false,
+			},
+		})
 
-    --keymaps
-    local keymap = vim.keymap
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>")
-  end
+		--keymaps
+		local keymap = vim.keymap
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>")
+	end,
 }
