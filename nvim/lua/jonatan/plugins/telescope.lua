73ed1799ba6ddc2e76bff2c1329d3945de3605ed
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-12-08 12:29:22 +0100

    added file ignores in telescope

--- nvim/lua/jonatan/plugins/telescope.lua
+++ nvim/lua/jonatan/plugins/telescope.lua
@@ -1,19 +1,27 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    
-    telescope.load_extension("fzf")
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-    --keymaps
-    local keymap = vim.keymap
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+		telescope.load_extension("fzf")
 
-  end
+		--keymaps
+		local keymap = vim.keymap
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+
+		telescope.setup({
+			defaults = {
+				file_ignore_patterns = {
+					"node_modules",
+					"target", -- Ignore rust build folder
+				},
+			},
+		})
+	end,
 }
