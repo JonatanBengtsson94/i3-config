62f93f213900c6fd42af81933e19a1e5b3bbff13
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2025-03-18 16:49:54 +0100

    added prettierd formatting

--- nvim/lua/jonatan/plugins/formatting.lua
+++ nvim/lua/jonatan/plugins/formatting.lua
@@ -1,22 +1,24 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-        conform.setup({
-            formatters_by_ft = {
-                javascript = { "prettier" },
-                html = { "prettier" },
-                css = { "prettier" },
-                json = { "prettier" },
-                lua = { "stylua" },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 1000,
-            }
-        })
-    end,
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				html = { "prettierd" },
+				css = { "prettierd" },
+				json = { "prettierd" },
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+	end,
 }
--- nvim/lua/jonatan/plugins/lsp/mason.lua
+++ nvim/lua/jonatan/plugins/lsp/mason.lua
@@ -31,6 +31,7 @@ return {
 		mason_tool_installer.setup({
 			ensure_installed = {
 				"prettier",
+				"prettierd",
 				"stylua",
 				"eslint_d",
 			},
